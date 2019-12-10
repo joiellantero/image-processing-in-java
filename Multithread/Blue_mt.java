b3da001d26e3ac60ff65e35988ab504285907f9f
jtllantero <joie.llantero@eee.upd.edu.ph>
2019-12-10 14:00:23 +0800

    updated multithread code

--- Multithread/Blue_mt.java
+++ Multithread/Blue_mt.java
@@ -1,133 +1,103 @@
 // package image_convert.bmp;
 package image.multithread; 
-
-import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
- 
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
 import javax.imageio.ImageIO;
+
+@SuppressWarnings("serial")
+public class Blue_mt extends RecursiveAction {
+    private int[] mSource;
+    private int mStart;
+    private int mLength;
+    private int[] mDestination;
+  
+    // Processing window size; should be odd.
+    private int mBlueWidth = 15;
+  
+    public Blue_mt(int[] src, int start, int length, int[] dst) {
+        mSource = src;
+        mStart = start;
+        mLength = length;
+        mDestination = dst;
+    }
+
+    protected void computeDirectly() {
+        int sidePixels = (mBlueWidth - 1) / 2;
+        for (int index = mStart; index < mStart + mLength; index++) {
+            // Calculate average.
+            float rt = 0, gt = 0, bt = 0;
+            for (int mi = -sidePixels; mi <= sidePixels; mi++) {
+                int mindex = Math.min(Math.max(mi + index, 0),
+                                    mSource.length - 1);
+                int pixel = mSource[mindex];
+                rt += (float)((pixel & 0x00ff0000) >> 16) / mBlueWidth;
+                gt += (float)((pixel & 0x0000ff00) >>  8) / mBlueWidth;
+                bt += (float)((pixel & 0x000000ff) >>  0) / mBlueWidth;
+            }
+          
+            // Reassemble destination pixel.
+            int dpixel = (0xff000000     ) |
+                   (((int)rt) << 16) |
+                   (((int)gt) <<  8) |
+                   (((int)bt) <<  0);
+            mDestination[index] = dpixel;
+        }
+    }
+
+    protected static int sThreshold = 100000;
+
+    @Override 
+    protected void compute() {
+        if (mLength < sThreshold) {
+            computeDirectly();
+            return;
+        }
+    
+        int split = mLength / 2;
+    
+        invokeAll(new Blue_mt(mSource, mStart, split, mDestination),
+                  new Blue_mt(mSource, mStart + split, mLength - split, mDestination));
+    }
+    public static void main(String args[])throws IOException 
+    { 
+        String srcName = "../Raw_Image/lena.jpg";
+        File srcFile = new File(srcName);
+        BufferedImage image = ImageIO.read(srcFile);
+
+        BufferedImage blueImage = blue(image);
+
+        String dstName = "../Processed_Images/lena_blue.jpg";
+        File dstFile = new File(dstName);
+        ImageIO.write(blueImage, "jpg", dstFile);
+
+    }  
+    
+    public static BufferedImage blue(BufferedImage srcImage) {
+        int w = srcImage.getWidth();
+        int h = srcImage.getHeight();
  
-class Multithread extends Thread{
-    public void run(){
-        BufferedImage image = null; 
-        File f = null; 
-  
-        // read image 
-        try
-        { 
-            f = new File("../Raw_Image/lena.jpg"); 
-            image = ImageIO.read(f); 
-        } 
-        catch(IOException e) 
-        { 
-            System.out.println(e); 
-        } 
-
-        int width = image.getWidth()/4; 
-        int height = image.getHeight()/4; 
-
-        Multithread (start, end){
-            return ();
-        }
-  
-        // convert to red image 
-        for (int y = 0; y < height; y++) 
-        { 
-            for (int x = 0; x < width; x++) 
-            { 
-                int rgb = image.getRGB(x,y); 
-  
-                int a = (rgb>>24)&0xff; 
-                int b = (rgb)&0xff; 
-  
-                // set new RGB (blue still the same and then 0 for red and green)
-                rgb = (a<<24) | (0<<16) | (0<<8) | b; 
-  
-                image.setRGB(x, y, rgb); 
-            } 
-        }
+        int[] src = srcImage.getRGB(0, 0, w, h, null, 0, w);
+        int[] dst = new int[src.length];
+ 
+        Blue_mt b = new Blue_mt(src, 0, src.length, dst);
+ 
+        ForkJoinPool pool = new ForkJoinPool();
+ 
+        long start = System.currentTimeMillis();
+        pool.invoke(b);
+        long end = System.currentTimeMillis();
+ 
+        float time = (end-start)/1000F;
+        System.out.println("Elapsed time: " + time + " s"); 
+ 
+        BufferedImage dstImage =
+                new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+        dstImage.setRGB(0, 0, w, h, dst, 0, w);
+ 
+        return dstImage;
     }
 }
 
-public class try_mt
-{      
-    static int w_total = 0;
-    static int h_total = 0;
-    static BufferedImage image = null;
-    static int totalTime = 0;
-   
-    public static void main( String[] args ) throws InterruptedException
-    {
-        try
-        {
-            image = ImageIO.read(new File("lena.jpg"));
-        }
-        
-            
-        catch (IOException e)
-        {
-            System.out.println(e);
-        }
-                
-        long start=System.currentTimeMillis();
-
-        // ImageMask mask1 = new ImageMask {
-        //     imageIn.getWidth(),         // width
-        //     imageIn.getHeight(),        // height
-        //     0,                          // x-start
-        //     0,                          // y-start
-        //     imageIn.getWidth(),         // region´s width
-        //     imageIn.getHeight()/2 
-        // }
-
-        // ImageMask mask2 = new ImageMask {
-        //     imageIn.getWidth(),         // width
-        //     imageIn.getHeight(),        // height
-        //     0,                          // x-start
-        //     imageIn.getHeight()/2,      // y-start
-        //     imageIn.getWidth(),         // region´s width
-        //     imageIn.getHeight()/2 
-        // }
-
-        int height = image.getHeight()/4; 
-
-        
-        Multithread object1 = new Multithread(0, height);
-        Multithread object2 = new Multithread(height, height*2);
-        Multithread object3 = new Multithread(height*2, height*3);
-        Multithread object4 = new Multithread(height*3, height*4);
-            
-        /* threads*/
-        object1.start();
-        object2.start();
-        object3.start();
-        object4.start();
-
-        object1.join();
-        object2.join();
-        object3.join();
-        object4.join();
-        
-        long stop=System.currentTimeMillis();
-
-            try
-            {
-                ImageIO.write(image, "jpg", new File("lena_new.jpg"));
-                System.out.println("End, saved");
-            }
-
-            catch (IOException e)
-            {
-                System.out.println(e);
-            }
-        
-        System.out.println("Total time: " + (stop-start));
-    }
-   
-    public static void setobject(int i, int j, Color bw_color)
-    {
-        image.setRGB(i, j, bw_color.getRGB());
-    }
-   
-}
